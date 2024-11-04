ee9747d738357eadf008bcc67462f8cc54297db3
curso00 <curso00@nomail.com>
2024-11-04 12:53:25 +0100

    Pepe ya sabe sumar

---
+++ src/main/java/es/santander/ascender/Pepe.java
@@ -0,0 +1,9 @@
+package es.santander.ascender;
+
+public class Pepe {
+    public int sumar(int a, int b) {
+        // TODO Cambiar el valor de retorno
+        //throw new UnsupportedOperationException("Aún no");
+        return a + b;
+    }
+}
